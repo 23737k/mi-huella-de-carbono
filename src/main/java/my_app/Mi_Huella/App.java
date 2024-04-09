8f4913e3999c3514e2d30b8ce28e4e41760c510d
kenti <016mki69@gmail.com>
2024-04-09 04:39:26 -0300

    First commit

---
+++ src/main/java/my_app/Mi_Huella/App.java
@@ -0,0 +1,16 @@
+package my_app.Mi_Huella;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        List<String> listaDeCadenas = null;
+    }
+}
---
+++ src/test/java/my_app/Mi_Huella/AppTest.java
@@ -0,0 +1,16 @@
+package my_app.Mi_Huella;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test
+	public void nuevoTest(){
+		assertEquals(2, 6+1);
+	}
+}
